82fa7d1f6e987a68892c61b79845240db98fc7bd
Souvik Das <souvikdas.uttarpara@gmail.com>
2022-04-11 10:39:06 +0530

    First Commit

---
+++ calculator.js
@@ -0,0 +1,18 @@
+let result = document.getElementById("inputext");
+let calculate = (number) => {
+    result.value = result.value + number;
+}
+let Result = () => {
+    try {
+        result.value = eval(result.value)
+    }
+    catch (err) {
+        alert("Enter a valid Argument.");
+    }
+}
+function clr() {
+    result.value = "";
+}
+function del() {
+    result.value = result.value.slice(0, -1);
+}
